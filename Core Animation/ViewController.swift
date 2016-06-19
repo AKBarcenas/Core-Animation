188a701edba73fd7a7843b99e1e7b700a54f662c
AKBarcenas <akbarcen@ucsd.edu>
2016-06-19 05:47:28 -0700

    Set up basic storyboard.

--- Core Animation/ViewController.swift
+++ Core Animation/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // The tap button in the view.
+    @IBOutlet weak var tap: UIButton!
+    // The image view that will be used to display the penguin.
+    var imageView: UIImageView!
+    // The current animation that is being used.
+    var currentAnimation = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tapped(sender: UIButton) {
+        currentAnimation += 1
+        
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+    }
 
 }
 
