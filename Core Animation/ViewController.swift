df4ccb9cb403ee4d2ddecdbdaf33e2239dd8c7c6
AKBarcenas <akbarcen@ucsd.edu>
2016-06-19 19:24:42 -0700

    Add comments

--- Core Animation/ViewController.swift
+++ Core Animation/ViewController.swift
@@ -16,6 +16,13 @@ class ViewController: UIViewController {
     // The current animation that is being used.
     var currentAnimation = 0
 
+    /*
+     * Function Name: viewDidLoad
+     * Parameters: None
+     * Purpose: This method adds a penguin to the view that the user can manipulate.
+     * Return Value: None
+     */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -30,6 +37,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    /*
+     * Function Name: tapped
+     * Parameters: sender - the button that called this method.
+     * Purpose: This method hides the button until an animation has been fully executed.
+     *   The method chooses animations sequentially based on a counter.
+     * Return Value: None
+     */
+    
     @IBAction func tapped(sender: AnyObject) {
         tap.hidden = true
         
@@ -37,32 +52,33 @@ class ViewController: UIViewController {
         //UIView.animateWithDuration(1, delay: 0, options: [],
                                    animations: { [unowned self] in
                                     switch self.currentAnimation {
+                                    // Scale the penguin up.
                                     case 0:
                                         self.imageView.transform = CGAffineTransformMakeScale(2, 2)
-                                        
+                                    // Reset the penguin.
                                     case 1:
                                         self.imageView.transform = CGAffineTransformIdentity
-                                        
+                                    // Move the penguin.
                                     case 2:
                                         self.imageView.transform = CGAffineTransformMakeTranslation(-256, -256)
-                                        
+                                    // Reset the penguin.
                                     case 3:
                                         self.imageView.transform = CGAffineTransformIdentity
-                                        
+                                    // Rotate the pengin 180 degrees.
                                     case 4:
                                         self.imageView.transform = CGAffineTransformMakeRotation(CGFloat(M_PI))
-                                        
+                                    // Reset the penguin.
                                     case 5:
                                         self.imageView.transform = CGAffineTransformIdentity
-                                        
+                                    // Change the tranparency and background color of the penguin.
                                     case 6:
                                         self.imageView.alpha = 0.1
                                         self.imageView.backgroundColor = UIColor.greenColor()
-                                        
+                                    // Reset the penguin.
                                     case 7:
                                         self.imageView.alpha = 1
                                         self.imageView.backgroundColor = UIColor.clearColor()
-                                        
+                                    // Do nothing.
                                     default:
                                         break
                                     }
@@ -71,7 +87,7 @@ class ViewController: UIViewController {
         }
         
         currentAnimation += 1
-        
+        // Reset the counter.
         if currentAnimation > 7 {
             currentAnimation = 0
         }
